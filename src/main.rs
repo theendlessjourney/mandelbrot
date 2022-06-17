55e61332a20d8f5c67c52bd876d9077dbbe05fb7
lhu@ds9arch <lhu@ds9arch>
2022-06-16 22:10:09 -0700

    got the colors working

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use minifb::{Key, ScaleMode, Window, WindowOptions};
 
-const WIDTH: usize = 640 / 2;
-const HEIGHT: usize = 360 / 2;
+const WIDTH: usize = 640;
+const HEIGHT: usize = 480;
 
 fn main() {
-    let mut noise;
-    let mut carry;
-    let mut seed = 0xbeefu32;
-
     let mut window = Window::new(
         "Noise Test - Press ESC to exit",
         WIDTH,
@@ -35,17 +31,35 @@ fn main() {
             println!("window size is {}x{}", size.0, size.1);
         }
 
-        for i in buffer.iter_mut() {
-            noise = seed;
-            noise >>= 3;
-            noise ^= seed;
-            carry = noise & 1;
-            noise >>= 1;
-            seed >>= 1;
-            seed |= carry << 30;
-            noise &= 0xFF;
-            *i = (noise << 16) | (noise << 8) | noise;
+        buffer.fill(0);
+
+        let left = size.0 - 1;
+        let bottom = size.1 - 1;
+        let center_x = left / 2;
+        let center_y = bottom / 2;
+
+        buffer[calc_buffer_index( &size, 0, 0 )] = u32_from_rgb( 0xff,0,0 );
+        buffer[calc_buffer_index( &size, left, 0 )] = u32_from_rgb( 0, 0xff, 0);
+        buffer[calc_buffer_index( &size, 0, bottom )] = u32_from_rgb(0xff, 0xff, 0);
+        buffer[calc_buffer_index( &size, left, bottom )] = u32_from_rgb(0xff,0xff,0xff);
+
+        for py in 0..size.1 {
+            for px in 0..size.0 {
+                let bufferIndex = calc_buffer_index( &size, px, py );
+                
+                let red = ( ( py as f32 / bottom as f32 ) * 255.0 ) as u32;
+                let green = ( ( px as f32 / left as f32 ) * 255.0 ) as u32;
+                //let blue = ( ( px as f32 / left as f32 ) * 255.0 ) as u32;
+                buffer[bufferIndex] = u32_from_rgb(red, green, 0);
+            }
         }
+
+        //for i in buffer.iter_mut() {
+
+            // *i = (r << 16) | (g << 8) | b;
+            // *i = 0;
+            // *i = 0xff;
+        // }
 
         window.get_keys().iter().for_each(|key| match key {
             Key::W => println!("holding w!"),
@@ -63,4 +77,17 @@ fn main() {
             .update_with_buffer(&buffer, new_size.0, new_size.1)
             .unwrap();
     }
+}
+
+fn calc_buffer_index( size: &(usize,usize), x: usize, y: usize ) -> usize
+{
+    let w = size.0;
+    // let h = size.1;
+
+    y * w + x
+}
+
+fn u32_from_rgb( r: u32, g: u32, b: u32 ) -> u32
+{
+    ( ( r & 0xff ) << 16 ) | ( ( g & 0xff ) << 8 ) | ( b & 0xff )
 }
